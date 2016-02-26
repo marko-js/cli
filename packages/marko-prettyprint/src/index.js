03e570d29b76eef83bff73229d99f9b84f9471d4
Patrick Steele-Idem <pnidem@gmail.com>
2016-02-26 10:38:09 -0700

    Initial commit

---
+++ packages/marko-prettyprint/src/index.js
@@ -0,0 +1,260 @@
+'use strict';
+
+const SYNTAX_CONCISE = 1;
+const SYNTAX_HTML = 2;
+
+function trimLinesStart(lines) {
+    var firstNonEmptyLine = -1;
+    for (var i=0; i<lines.length; i++) {
+        if (lines[i].trim()) {
+            firstNonEmptyLine = i;
+            break;
+        }
+    }
+
+    if (firstNonEmptyLine > 0) {
+        lines = lines.slice(firstNonEmptyLine);
+    }
+
+    return lines;
+}
+
+function trimLinesEnd(lines) {
+    var firstNonEmptyLine = -1;
+    for (var i=lines.length-1; i>=0; i--) {
+        if (lines[i].trim()) {
+            firstNonEmptyLine = i;
+            break;
+        }
+    }
+
+    if (firstNonEmptyLine < lines.length-1) {
+        lines = lines.slice(0, firstNonEmptyLine + 1);
+    }
+
+    return lines;
+}
+
+module.exports = function prettyPrint(ast, options) {
+    var src = '';
+    var indent = '    ';
+    var currentIndent = '';
+    var syntax = options && options.syntax === 'concise' ?
+        SYNTAX_CONCISE :
+        SYNTAX_HTML;
+
+    var bufferedText = '';
+
+    function writeLineIndent() {
+        src += currentIndent;
+    }
+
+    function write(str) {
+        src += str;
+    }
+
+    function incIndent() {
+        currentIndent += indent;
+    }
+
+    function decIndent() {
+        currentIndent = currentIndent.substring(indent.length);
+    }
+
+    function indentLines(lines) {
+        let blockIndentation = null;
+        return lines.map((line) => {
+            if (blockIndentation == null) {
+                blockIndentation = line.match(/^\s*/)[0];
+            }
+
+            if (line.startsWith(blockIndentation)) {
+                line = line.substring(blockIndentation.length);
+                line = currentIndent + line;
+            } else {
+                line = currentIndent + line.trim();
+            }
+            return line;
+        });
+    }
+
+    function flushText() {
+        // bufferedText = bufferedText.trim();
+
+        if (!bufferedText) {
+            return;
+        }
+
+        var lines = bufferedText.split(/\n|\r\n/);
+
+        lines = trimLinesStart(lines);
+        lines = trimLinesEnd(lines);
+
+        if (lines.length === 0) {
+            return;
+        }
+
+        if (syntax === SYNTAX_CONCISE || currentIndent === '') {
+            if (lines.length > 1) {
+                lines = indentLines(lines);
+                write(currentIndent + '---\n' + lines.join('\n') + '\n' + currentIndent + '---');
+            } else {
+                write(currentIndent + '- ' + lines[0].trim());
+            }
+        } else {
+            if (lines.length > 1) {
+                lines = indentLines(lines);
+                write(lines.join('\n'));
+            } else {
+                write(currentIndent + lines[0].trim());
+            }
+        }
+
+        write('\n');
+
+        bufferedText = '';
+    }
+
+    function printHtmlElement(node) {
+        flushText();
+
+
+        var col = 0;
+
+        writeLineIndent();
+        col += currentIndent.length;
+
+        if (syntax === SYNTAX_HTML) {
+            write('<');
+            col++;
+        }
+
+        write(node.tagName);
+        col += node.tagName.length;
+
+        if (node.argument) {
+            write('(' + node.argument + ')');
+            col += node.argument.length + 2;
+        }
+
+        col++; // Allow for a space after the tag name
+
+        var multilineAttrs = false;
+
+        var attrs = node.getAttributes();
+        var attrsStr = '';
+
+        attrs.forEach((attr, i) => {
+
+            var attrStr = '';
+
+            if (attr.name) {
+                attrStr += attr.name;
+                var attrValue = attr.value;
+                if (attrValue) {
+                    if (attrValue.isCompoundExpression()) {
+                        attrStr += '=(' + attrValue + ')';
+                    }else {
+                        attrStr += '=' + attrValue;
+                    }
+
+                } else if (attr.argument) {
+                    attrStr += '(' + attr.argument + ')';
+                }
+            } else {
+                attrStr += '${' + attr.value  + '}';
+            }
+
+            console.log('attrStr', JSON.stringify(attrStr), col + attrStr.length);
+
+            if (i !== 0 && col + attrStr.length > 80 && attrStr.length < (80 - currentIndent.length + indent.length)) {
+                attrsStr += '\n' + currentIndent + indent + attrStr;
+                col = currentIndent.length + indent.length + attrStr.length;
+                multilineAttrs = true;
+            } else {
+                if (attrsStr.length) {
+                    attrStr = ' ' + attrStr;
+                }
+                attrsStr += attrStr;
+                col += attrStr.length;
+            }
+        });
+
+        if (attrsStr) {
+            write(' ');
+
+            if (multilineAttrs && syntax === SYNTAX_CONCISE) {
+                write('[ ');
+            }
+
+            write(attrsStr);
+
+            if (multilineAttrs && syntax === SYNTAX_CONCISE) {
+                write(' ]');
+            }
+        }
+
+        var hasBody = node.body && node.body.length;
+        if (syntax === SYNTAX_HTML) {
+            if (hasBody) {
+                write('>');
+            } else {
+                write('/>');
+            }
+        }
+
+        write('\n');
+
+        if (hasBody && multilineAttrs) {
+            write('\n'); // Add one more extra spacing line if the element has multiline attributes
+        }
+
+        if (hasBody) {
+            incIndent();
+
+            node.body.forEach((child) => {
+                printNode(child);
+            });
+
+            flushText();
+
+            decIndent();
+
+            if (multilineAttrs && syntax !== SYNTAX_CONCISE) {
+                write('\n'); // Keep the spacing symmetrical
+            }
+        }
+
+        if (hasBody && syntax === SYNTAX_HTML) {
+            writeLineIndent();
+            write('</');
+            write(node.tagName);
+            write('>\n');
+        }
+    }
+
+    function printText(node) {
+        bufferedText += node.argument.value;
+    }
+
+    function printNode(node, isRoot) {
+        switch (node.type) {
+            case 'HtmlElement':
+                printHtmlElement(node);
+                break;
+            case 'Text':
+                printText(node);
+                break;
+            default:
+                throw new Error('Unsupported node: ' + node);
+        }
+    }
+
+    ast.body.forEach((child) => {
+        printNode(child);
+    });
+
+    flushText();
+
+    return src;
+};
