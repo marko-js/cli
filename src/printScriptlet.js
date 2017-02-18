3f3cb531217974b039a628e9ffc5977c622a0013
austinkelleher <austin.kell47@gmail.com>
2017-02-18 09:59:57 -0500

    Fixes #10 - Add support for scriptlets.

---
+++ src/printScriptlet.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const indentLines = require('./util/indent').indentLines;
+
+function getLines(code) {
+    return code.split(/\r\n|\n/);
+}
+
+module.exports = function printScriptlet(node, printContext, writer) {
+    const currentIndentString = printContext.currentIndentString;
+    const eol = printContext.eol;
+
+    let code = node.code;
+    let lines = getLines(code);
+
+    if (code.startsWith('\n')) {
+        // Multi-line scriptlet
+        let indentedLines = indentLines(lines, printContext);
+        writer.write(currentIndentString + '$ {' + indentedLines.join(eol) + currentIndentString + '}');
+    } else if (lines.length > 1) {
+        writer.write(currentIndentString + '$ ');
+        // Write the first line then indent the following lines and write them
+        // This scriptlet is in the following format:
+        // $ var test = {
+        //
+        // }
+        let indentedLines = indentLines(lines, printContext);
+        writer.write(currentIndentString + indentedLines.join(eol) +  eol + currentIndentString);
+    } else {
+        // Single-line scriptlet
+        writer.write('$ ');
+        writer.write(code);
+    }
+};
