f6bdc4e524a5893d084df8623ded905506bbfebb
Patrick Steele-Idem <pnidem@gmail.com>
2017-02-22 15:16:06 -0700

    Prettyprint legacy scriptlets correctly

--- packages/marko-prettyprint/src/printScriptlet.js
+++ packages/marko-prettyprint/src/printScriptlet.js
@@ -13,22 +13,29 @@ module.exports = function printScriptlet(node, printContext, writer) {
     let code = node.code;
     let lines = getLines(code);
 
-    if (code.startsWith('\n')) {
-        // Multi-line scriptlet
-        let indentedLines = indentLines(lines, printContext);
-        writer.write(currentIndentString + '$ {' + indentedLines.join(eol) + currentIndentString + '}');
-    } else if (lines.length > 1) {
-        writer.write(currentIndentString + '$ ');
-        // Write the first line then indent the following lines and write them
-        // This scriptlet is in the following format:
-        // $ var test = {
-        //
-        // }
-        let indentedLines = indentLines(lines, printContext);
-        writer.write(currentIndentString + indentedLines.join(eol) +  eol + currentIndentString);
-    } else {
-        // Single-line scriptlet
-        writer.write('$ ');
+    if (node.tag) {
+        writer.write('<%');
         writer.write(code);
+        writer.write('%>');
+    } else {
+        if (code.startsWith('\n')) {
+            // Multi-line scriptlet
+            let indentedLines = indentLines(lines, printContext);
+            writer.write(currentIndentString + '$ {' + indentedLines.join(eol) + currentIndentString + '}');
+        } else if (lines.length > 1) {
+            writer.write(currentIndentString + '$ ');
+            // Write the first line then indent the following lines and write them
+            // This scriptlet is in the following format:
+            // $ var test = {
+            //
+            // }
+            let indentedLines = indentLines(lines, printContext);
+            writer.write(currentIndentString + indentedLines.join(eol) +  eol + currentIndentString);
+        } else {
+            // Single-line scriptlet
+            writer.write('$ ');
+            writer.write(code);
+        }
     }
+
 };
