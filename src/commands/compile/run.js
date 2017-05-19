7fca689e85f9cc96d02913c98dc99b81f9dac7eb
Michael Rawlings <ml.rawlings@gmail.com>
2017-05-19 11:44:00 -0700

    fix some issues with paths when compiling and generate browser remaps in package.json

--- src/commands/compile/run.js
+++ src/commands/compile/run.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var glob = require('glob');
+var path = require('path');
 
 var globOptions = {
     matchBase: true,
@@ -11,6 +12,7 @@ var globOptions = {
 
 module.exports = function run(options, devTools) {
     var markoCompiler = devTools.requireFromRoot('marko/compiler');
+    var packageRoot = devTools.packageRoot;
 
     globOptions.cwd = devTools.cwd;
 
@@ -19,30 +21,77 @@ module.exports = function run(options, devTools) {
             glob(pattern, globOptions, function (err, files) {
                 if (err) return reject(err);
 
+                var packagePath = path.join(packageRoot, 'package.json');
+                var packageData = {};
+
+                try {
+                    packageData = require(packagePath);
+                } catch(e) {}
+
+                packageData.browser = packageData.browser || {};
+
+                if (!options.clean) {
+                    files.map((file) => {
+                        var serverFile, browserFile;
+                        if (options.server) {
+                            serverFile = file.replace(/\.\w+$/, '.js');
+                            createIfNotExists(serverFile);
+                        }
+                        if (options.browser && markoCompiler.compileFileForBrowser) {
+                            browserFile = file.replace(/\.\w+$/, '.browser.js');
+                            createIfNotExists(browserFile);
+                        }
+                    });
+                }
+
                 files.map((file) => {
+                    var serverFile, browserFile;
                     if (options.server) {
-                        var targetFile = file.replace(/\.\w+$/, '.js');
+                        serverFile = file.replace(/\.\w+$/, '.js');
                         if (options.clean) {
-                            fs.unlinkSync(targetFile);
+                            try {
+                                fs.unlinkSync(serverFile);
+                            } catch(e) {}
                         } else {
                             var compiledSrc = markoCompiler.compileFile(file);
-                            fs.writeFileSync(targetFile, compiledSrc);
+                            fs.writeFileSync(serverFile, compiledSrc);
                         }
                     }
                     if (options.browser && markoCompiler.compileFileForBrowser) {
-                        var targetFile = file.replace(/\.\w+$/, '.browser.js');
+                        browserFile = file.replace(/\.\w+$/, '.browser.js');
                         if (options.clean) {
-                            fs.unlinkSync(targetFile);
+                            try {
+                                fs.unlinkSync(browserFile);
+                            } catch(e) {}
                         } else {
                             var compiled = markoCompiler.compileFileForBrowser(file);
-                            fs.writeFileSync(targetFile, compiled.code);
+                            fs.writeFileSync(browserFile, compiled.code);
+                        }
+                    }
+
+                    if (serverFile && browserFile) {
+                        if (options.clean) {
+                            delete packageData.browser[path.relative(packageRoot, serverFile)];
+                        } else {
+                            packageData.browser[path.relative(packageRoot, serverFile)] = path.relative(packageRoot, browserFile);
                         }
                     }
                 });
 
+                if (!Object.keys(packageData.browser).length) {
+                    delete packageData.browser;
+                }
+
+                fs.writeFileSync(packagePath, JSON.stringify(packageData, null, 2)+'\n');
 
                 resolve();
             });
         }))
     );
 };
+
+function createIfNotExists(filepath) {
+    if (!fs.existsSync(filepath)) {
+        //fs.writeFileSync(filepath, {flag: 'wx'});
+    }
+}
