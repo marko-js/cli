af89130ac0eb3b426b165a49a67c28dbcaf1d9df
Dylan Piercey <dpiercey@ebay.com>
2018-12-04 12:52:58 -0800

    chore: upgrade dev deps chore: switch to babel 7 setup chore: add commit linting / changelog generation

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/env",
+      {
+        loose: true,
+        targets: {
+          node: "8"
+        }
+      }
+    ]
+  ],
+  plugins: [
+    "@babel/transform-runtime",
+    "@babel/proposal-object-rest-spread"
+  ],
+  env: {
+    test: {
+      plugins: ["babel-plugin-istanbul"]
+    }
+  }
+};
