36781e604996b99b51925b1c8ae25b133dfa1604
Michael Rawlings <mirawlings@ebay.com>
2018-12-04 18:05:09 -0800

    pull cli interface out into individual packages

--- packages/cli/src/Commands.js
+++ packages/cli/src/Commands.js
@@ -11,12 +11,12 @@ class Commands {
 
     commandNames.forEach(commandName => {
       function run() {
-        var runFunc = require(`./commands/${commandName}/run`);
-        return runFunc.apply(null, arguments);
+        var cli = require(`@marko/${commandName}/dist/cli`);
+        return cli.run.apply(null, arguments);
       }
       function parse() {
-        var parseFunc = require(`./commands/${commandName}/parse`);
-        return parseFunc.apply(null, arguments);
+        var cli = require(`@marko/${commandName}/dist/cli`);
+        return cli.parse.apply(null, arguments);
       }
       this.add(commandName, {
         run,
