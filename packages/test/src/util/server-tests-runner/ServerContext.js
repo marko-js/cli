fd4c6adee79e2c23f59dc3a3902a251014341ce2
Dylan Piercey <dpiercey@ebay.com>
2019-02-06 12:13:48 -0800

    feat(test): add renderAsync method to server context

--- packages/test/src/util/server-tests-runner/ServerContext.js
+++ packages/test/src/util/server-tests-runner/ServerContext.js
@@ -47,26 +47,50 @@ class ServerContext {
       htmlString = this.component.render(data).toString();
     }
 
-    var snapshotId = this._nextSnapshotId++;
-    let snapshotFile =
-      this.name.replace(/[^A-Za-z0-9_\-.]/g, "-") + "." + snapshotId + ".html";
-    let testDir = path.join(this.componentDir, "test");
-    let snapshotDir = path.join(this.componentDir, "test/snapshots");
-
-    // Snapshots should always be placed in /test/snapshots even if the test file is located
-    // at the component level
-    if (!fs.existsSync(testDir)) {
-      fs.mkdirSync(testDir);
-    }
-
-    if (!fs.existsSync(snapshotDir)) {
-      fs.mkdirSync(snapshotDir);
-    }
-
-    fs.writeFileSync(path.join(snapshotDir, snapshotFile), htmlString);
-
+    createSnapshot(
+      this.name,
+      this._nextSnapshotId++,
+      this.componentDir,
+      htmlString
+    );
     return new RenderResult(htmlString);
   }
+
+  renderAsync(data) {
+    return new Promise((resolve, reject) =>
+      this.component.render(data, (err, result) =>
+        err ? reject(err) : resolve(result)
+      )
+    ).then(result => {
+      var htmlString = String(result);
+      createSnapshot(
+        this.name,
+        this._nextSnapshotId++,
+        this.componentDir,
+        htmlString
+      );
+      return new RenderResult(htmlString);
+    });
+  }
+}
+
+function createSnapshot(name, id, componentDir, html) {
+  const snapshotFile =
+    name.replace(/[^A-Za-z0-9_\-.]/g, "-") + "." + id + ".html";
+  const testDir = path.join(componentDir, "test");
+  const snapshotDir = path.join(componentDir, "test/snapshots");
+
+  // Snapshots should always be placed in /test/snapshots even if the test file is located
+  // at the component level
+  if (!fs.existsSync(testDir)) {
+    fs.mkdirSync(testDir);
+  }
+
+  if (!fs.existsSync(snapshotDir)) {
+    fs.mkdirSync(snapshotDir);
+  }
+
+  fs.writeFileSync(path.join(snapshotDir, snapshotFile), html);
 }
 
 module.exports = ServerContext;
