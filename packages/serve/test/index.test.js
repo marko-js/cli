fbc60a2fe560da5a030ec4a8edd7797939baf128
Michael Rawlings <mirawlings@ebay.com>
2019-07-10 17:34:15 -0700

    test(serve): assert on the html rather than screenshot pixels

--- packages/serve/test/index.test.js
+++ packages/serve/test/index.test.js
@@ -1,15 +1,10 @@
 import fs from "fs";
-import path from "path";
 import autotest from "mocha-autotest";
 import puppeteer from "puppeteer";
-import pixelmatch from "pixelmatch";
 import cluster from "cluster";
 import { copy, remove } from "fs-extra";
-import { PNG } from "pngjs";
 import { run } from "../src/cli";
 import build from "../../build/src/index";
-
-const updateExpectations = process.env.hasOwnProperty("UPDATE_EXPECTATIONS");
 
 describe("scope(serve)", function() {
   this.slow(20000);
@@ -80,7 +75,13 @@ function createTest(createServer) {
         closeServer = await createServer(options, { resolve });
         browser = await puppeteer.launch();
         const page = await browser.newPage();
-        const screenshot = screenshotUtility.bind(null, page, resolve);
+        const screenshot = screenshotUtility.bind(
+          null,
+          page,
+          mode,
+          snapshot,
+          resolve
+        );
         await page.goto(
           `http://localhost:${options.port}${(main && main.path) || "/"}`
         );
@@ -110,40 +111,13 @@ function createTest(createServer) {
   };
 }
 
-async function screenshotUtility(page, resolve, name, element) {
-  const target = element || page;
-  const actualPath = resolve(`${name ? `${name}-` : ""}actual.png`);
-  const expectedPath = resolve(`${name ? `${name}-` : ""}expected.png`);
-  const diffPath = resolve(`${name ? `${name}-` : ""}diff.png`);
-  await target.screenshot({ path: actualPath, fullPage: target === page });
-  if (!fs.existsSync(expectedPath) || updateExpectations) {
-    fs.copyFileSync(actualPath, expectedPath);
-  } else {
-    const actual = PNG.sync.read(fs.readFileSync(actualPath));
-    const expected = PNG.sync.read(fs.readFileSync(expectedPath));
-    const { width, height } = actual;
-    const diff = new PNG({ width, height });
-
-    pixelmatch(actual.data, expected.data, diff.data, width, height, {
-      threshold: 0.1,
-      diffColor: [255, 0, 0]
-    });
-
-    fs.writeFileSync(diffPath, PNG.sync.write(diff));
-
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        const idx = (width * y + x) << 2;
-        const red = diff.data[idx + 0];
-        const green = diff.data[idx + 1];
-        const blue = diff.data[idx + 2];
-        const alpha = diff.data[idx + 3];
-        if (red === 255 && green === 0 && blue === 0 && alpha === 255) {
-          throw new Error(
-            `ScreenshotMismatch: ${path.relative(process.cwd(), diffPath)}`
-          );
-        }
-      }
-    }
-  }
+async function screenshotUtility(page, mode, snapshot, resolve, name, element) {
+  const target = element || (await page.$("body"));
+  const nameWithMode = `${name || ""}${name && mode ? "-" : ""}${mode || ""}`;
+  const screenshotPath = resolve(
+    `${nameWithMode && `${nameWithMode}-`}actual.png`
+  );
+  const html = await page.evaluate(el => el.outerHTML, target);
+  await target.screenshot({ path: screenshotPath });
+  snapshot(html, { name: nameWithMode, ext: ".html" });
 }
