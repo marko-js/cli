740909d3b40bb7cc12aec1c44cab959f61e6d7ff
Michael Rawlings <mirawlings@ebay.com>
2020-05-06 15:57:59 -0700

    fix: update-notifier takes pkg key

--- packages/create/src/bin.js
+++ packages/create/src/bin.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 const updateNotifier = require("update-notifier");
 const { parse, run } = require("./cli");
-updateNotifier(require("../package.json")).notify();
+updateNotifier({ pkg: require("../package.json") }).notify();
 run(parse(process.argv.slice(2)));
