34b9e76d76aed6fc763ebea2829d27d308051849
Dylan Piercey <dpiercey@ebay.com>
2020-07-21 13:42:34 -0700

    feat(build): marko 5 support for src attributes transform
    BREAKING CHANGE: resolving asset paths now use nodes resolution only

--- packages/build/src/components/src-attributes-transformer.js
+++ packages/build/src/components/src-attributes-transformer.js
@@ -1,24 +1,4 @@
-const resolveFrom = require("resolve-from");
-
-module.exports = function srcAttributes(el, context) {
-  if (tagAttrs[el.tagName])
-    el.attributes.forEach(attr => {
-      if (!tagAttrs[el.tagName + ":" + attr.name]) return;
-
-      var walker = context.createWalker({
-        enter: node => {
-          let nodeHandler = nodeHandlers[node.type];
-
-          if (!nodeHandler) return walker.skip();
-          if (nodeHandler === true) return;
-
-          return nodeHandler(node, walker, context);
-        }
-      });
-
-      attr.value = walker.walk(attr.value);
-    });
-};
+"use strict";
 
 const attrTags = {
   src: [
@@ -35,58 +15,107 @@ const attrTags = {
   href: ["a", "area", "link"],
   data: ["object"],
   poster: ["video"],
-  srcset: ["img"], //something else needs to happen here
+  srcset: ["img"],
+  //something else needs to happen here
   background: ["body"]
 };
-
 const tagAttrs = Object.keys(attrTags).reduce((tagAttrs, attrName) => {
   attrTags[attrName].forEach(tagName => {
-    tagAttrs[tagName] = true;
-    tagAttrs[tagName + ":" + attrName] = true;
+    tagAttrs[tagName] = tagAttrs[tagName] || {};
+    tagAttrs[tagName][attrName] = true;
   });
   return tagAttrs;
 }, {});
 
-const nodeHandlers = {
-  ArrayExpression: true,
-  ObjectExpression: true,
-  Property: true,
-  LogicalExpression: true,
-  ConditionalExpression: (node, walker) => {
-    node.consequent = walker.walk(node.consequent);
-    node.alternate = walker.walk(node.alternate);
-    walker.skip();
-  },
-  Literal: (node, walker, context) => {
-    const modulePath = getRelativeModulePath(node.value, context.dirname);
-    if (modulePath) {
-      const importedSrcValue = addSrcImport(context, modulePath);
-      walker.replace(importedSrcValue);
-    }
+module.exports = function(a, b) {
+  if (a.hub) {
+    return transformMarko5(a, b);
   }
+
+  return transformMarko4(a, b);
 };
 
-const protocolPattern = /^[a-z]{2,}:/i;
+function transformMarko5(path) {
+  if (!path.get("name").isStringLiteral()) {
+    return;
+  }
 
-function getRelativeModulePath(path, dirname) {
-  if (!path) return false;
-  if (typeof path !== "string") return false;
-  if (protocolPattern.test(path)) return false;
-  return (
-    resolveFrom.silent(dirname, path) ||
-    resolveFrom.silent(dirname, "./" + path)
-  );
+  const tagName = path.get("name.value").node;
+  const checkAttrs = tagAttrs[tagName];
+  if (!checkAttrs) {
+    return;
+  }
+
+  path.get("attributes").forEach(attr => {
+    if (!checkAttrs[attr.get("name").node]) {
+      return;
+    }
+
+    const { confident, value } = attr.get("value").evaluate();
+
+    if (!confident || !isAssetPath(value)) {
+      return;
+    }
+
+    attr.set("value", path.hub.file.importDefault(attr, value, "asset"));
+  });
 }
 
-function addSrcImport(context, path) {
-  context.assetCount = context.assetCount || 0;
-  const varName = `__src_asset_${context.assetCount++}__`;
-  const tagString = `import ${varName} from ${JSON.stringify(path)}`;
-  const importTag = context.createNodeForEl("import");
+function transformMarko4(el, context) {
+  const checkAttrs = tagAttrs[el.tagName];
+  if (!checkAttrs) {
+    return;
+  }
 
-  importTag.tagString = tagString;
+  el.attributes.forEach(attr => {
+    if (!checkAttrs[attr.name]) {
+      return;
+    }
 
-  context.root.prependChild(importTag);
+    const walker = context.createWalker({
+      enter: node => {
+        switch (node.type) {
+          case "ArrayExpression":
+          case "ObjectExpression":
+          case "Property":
+          case "LogicalExpression":
+            return;
+          case "ConditionalExpression":
+            node.consequent = walker.walk(node.consequent);
+            node.alternate = walker.walk(node.alternate);
+            walker.skip();
+            break;
+          case "Literal": {
+            const { value } = node;
 
-  return context.builder.identifier(varName);
+            if (!isAssetPath(value)) {
+              return;
+            }
+
+            context.assetCount = context.assetCount || 0;
+            const varName = `__src_asset_${context.assetCount++}__`;
+            const tagString = `import ${varName} from ${JSON.stringify(value)}`;
+            const importTag = context.createNodeForEl("import");
+            importTag.tagString = tagString;
+            context.root.prependChild(importTag);
+            walker.replace(context.builder.identifier(varName));
+            break;
+          }
+          default:
+            walker.skip();
+            break;
+        }
+      }
+    });
+
+    attr.value = walker.walk(attr.value);
+  });
+}
+
+function isAssetPath(relativePath) {
+  if (typeof relativePath !== "string") return false;
+  if (relativePath[0] === "/") return false; // Ignore absolute paths.
+  if (!/\.[^.]+$/.test(relativePath)) return false; // Ignore paths without a file extension.
+  if (/^[a-z]{2,}:/i.test(relativePath)) return false; // Ignore paths with a protocol.
+  return true;
 }
