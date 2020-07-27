c9120d2d1be35ac0808168b35109f3637b9703d7
Dylan Piercey <dpiercey@ebay.com>
2020-07-27 15:50:18 -0700

    fix(prettyprint): ensure supported copy of Marko is used

--- packages/prettyprint/src/util/requireMarkoFile.js
+++ packages/prettyprint/src/util/requireMarkoFile.js
@@ -2,20 +2,23 @@ const path = require("path");
 const resolveFrom = require("resolve-from");
 
 function requireMarkoFile(dir, file) {
-  const requirePath = `marko/${file}`;
-  let resolvedPath =
-    resolveFrom(dir, requirePath) || require.resolve(requirePath);
-  if (resolvedPath) {
-    return require(resolvedPath);
-  } else {
-    return require(requirePath);
+  const { version } = requireFromDirOrFallback(dir, "marko/package.json");
+
+  if (version.split(".")[0] === "4") {
+    return requireFromDirOrFallback(dir, `marko/${file}`);
   }
+
+  return require(`marko/${file}`);
 }
 
 function getMarkoCompiler(dir, file) {
   const isDebug = requireMarkoFile(dir, "env").isDebug;
   const requirePath = path.join(isDebug ? "src" : "dist", file);
   return requireMarkoFile(dir, requirePath);
+}
+
+function requireFromDirOrFallback(dir, file) {
+  return require(resolveFrom.silent(dir, file) || file);
 }
 
 module.exports = getMarkoCompiler;
